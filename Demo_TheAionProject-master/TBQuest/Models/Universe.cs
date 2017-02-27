8d04a5c74740a03962048bdafc31c8b9625a4043
Ryan Parlin <parlinr@ad.nmc.edu>
2017-02-27 17:53:18 -0500

    Remove other projects, begin work on game universe

---
+++ Demo_TheAionProject-master/TBQuest/Models/Universe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBQuest
+{
+    /// <summary>
+    /// game area class
+    /// </summary>
+    public class Universe
+    {
+        #region Lists maintained by Universe object
+
+        #endregion
+
+        #region Constructor
+        //default constructor
+        public Universe()
+        {
+            //add universe objects to game
+            InitializeUniverse();
+        }
+        #endregion
+
+        #region Methods
+        private void InitializeUniverse()
+        {
+
+        }
+        #endregion
+    }
+}
