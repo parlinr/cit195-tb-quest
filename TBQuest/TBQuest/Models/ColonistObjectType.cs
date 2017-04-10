562a8c1a846dc86d6246a290194bc386717c4e56
Ryan Parlin <parlinr@ad.nmc.edu>
2017-04-10 17:41:45 -0400

    Minor changes

---
+++ TBQuest/TBQuest/Models/ColonistObjectType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBQuest
+{
+    public enum ColonistObjectType
+    {
+        None,
+        Food,
+        Medicine,
+        Weapon,
+        Treasure,
+        Information
+    }
+}
